7c67048cd4c00d429ae04e83cd6ffd0f9b94dacf
kevin <kevin.hernandez1802@gmail.com>
2019-09-10 19:40:59 -0500

    done

--- server/index.js
+++ server/index.js
@@ -1,37 +1,42 @@
-const express = require ('express')
-const session = require ('express-session')
-const swagController = require('./controllers/swagController')
-const authController = require('./controllers/authController')
-const cartController = require ('./controllers/cartController')
-const searchController =require ('./controllers/searchController')
-require('dotenv').config()
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const checkForSession = require("./middlewares/checkForSession");
+const swagController = require("./controllers/swagController");
+const authController = require("./controllers/authController");
+const cartController = require("./controllers/cartController");
+const searchController = require("./controllers/searchController");
 
-const app = express()
+const app = express();
 
-let{SERVER_PORT,SESSION_SECRET}=process.env
-//midware
-app.use(express.json())
+let { SERVER_PORT, SESSION_SECRET } = process.env;
+
+// Middleware
+app.use(express.json());
 app.use(
-    session({
-        secret:SESSION_SECRET,
-        resave:false,
-        saveUninitialized:true
-    })
-)
-//endpoints
-app.get("/api/swag",swagController.read)
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+);
+app.use(checkForSession);
 
-app.post("/api/register",authController.register)
-app.post("/api/login",authController.login)
-app.post('/api/signout',authController.signout)
-app.get("/api/user",authController.getUser)
+// Endpoints
+//// Auth
+app.post("/api/register", authController.register);
+app.post("/api/login", authController.login);
+app.post("/api/signout", authController.signout);
+app.get("/api/user", authController.getUser);
+//// Swag
+app.get("/api/swag", swagController.read);
+//// Cart
+app.post("/api/cart/checkout", cartController.checkout);
+app.post("/api/cart/:id", cartController.add);
+app.delete("/api/cart/:id", cartController.delete);
+// Search
+app.get("/api/search", searchController.search);
 
-app.post("/api/cart/checkout",cartController.checkout) 
-app.post("/api/cart/:id",cartController.add)
-app.delete("/api/cart/id:",cartController.delete)
-
-app.get("/api/search/",searchController.search)
-
-app.listen(SERVER_PORT,()=>{
-    console.log(`server listening on port ${SERVER_PORT}.`)
-})
+app.listen(SERVER_PORT, () => {
+  console.log(`Server listening on port ${SERVER_PORT}.`);
+});
